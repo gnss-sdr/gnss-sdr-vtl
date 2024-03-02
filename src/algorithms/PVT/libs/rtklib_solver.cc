c68f3fda015986a1e703ddb5e3b86d2c8baeca07
miguekf <angel.glopez@alumnos.upm.es>
2024-03-02 16:38:08 +0100

    [ADD] res_pv on pvt_kf

--- src/algorithms/PVT/libs/rtklib_solver.cc
+++ src/algorithms/PVT/libs/rtklib_solver.cc
@@ -1552,10 +1552,16 @@ bool Rtklib_Solver::get_PVT(const std::map<int, Gnss_Synchro> &gnss_observables_
                                 {
                                     arma::vec p = {pvt_sol.rr[0], pvt_sol.rr[1], pvt_sol.rr[2]};
                                     arma::vec v = {pvt_sol.rr[3], pvt_sol.rr[4], pvt_sol.rr[5]};
+                                    arma::vec res_pv = {pvt_sol.qr[0], pvt_sol.qr[1], pvt_sol.qr[2],
+                                        pvt_sol.qr[3], pvt_sol.qr[4], pvt_sol.qr[5], pvt_sol.qvr[0], pvt_sol.qvr[1], pvt_sol.qvr[2],
+                                        pvt_sol.qvr[3], pvt_sol.qvr[4], pvt_sol.qvr[5]};
 
                                     d_pvt_kf.init_Kf(p,
                                         v,
+                                        res_pv,
                                         kf_update_interval_s,
+                                        d_conf.static_scenario_sd,
+                                        d_conf.estatic_measures_sd,
                                         d_conf.measures_ecef_pos_sd_m,
                                         d_conf.measures_ecef_vel_sd_ms,
                                         d_conf.system_ecef_pos_sd_m,
@@ -1565,7 +1571,11 @@ bool Rtklib_Solver::get_PVT(const std::map<int, Gnss_Synchro> &gnss_observables_
                                 {
                                     arma::vec p = {pvt_sol.rr[0], pvt_sol.rr[1], pvt_sol.rr[2]};
                                     arma::vec v = {pvt_sol.rr[3], pvt_sol.rr[4], pvt_sol.rr[5]};
-                                    d_pvt_kf.run_Kf(p, v);
+                                    arma::vec res_pv = {pvt_sol.qr[0], pvt_sol.qr[1], pvt_sol.qr[2],
+                                        pvt_sol.qr[3], pvt_sol.qr[4], pvt_sol.qr[5], pvt_sol.qvr[0], pvt_sol.qvr[1], pvt_sol.qvr[2],
+                                        pvt_sol.qvr[3], pvt_sol.qvr[4], pvt_sol.qvr[5]};
+
+                                    d_pvt_kf.run_Kf(p, v, res_pv);
                                     d_pvt_kf.get_pv_Kf(p, v);
                                     pvt_sol.rr[0] = p[0];  // [m]
                                     pvt_sol.rr[1] = p[1];  // [m]
